d19afbcdf389d4fb0f6dd7423243d33b16813793
Jacob Mink <jacobmink@gmail.com>
2019-01-14 23:37:16 -0700

    Still can't figure out how to link images

--- models/marsMissions.js
+++ models/marsMissions.js
@@ -4,35 +4,35 @@ const marsMissions = [
       launchDate: "26 Nov 2011",
       operator: "NASA",
       missionType: "Rover",
-      img: ""
+      img: "curiosity.jpg"
     },
     {
       name: "Opportunity",
       launchDate: "8 Jul 2003",
       operator: "NASA",
       missionType: "Rover",
-      img: ""
+      img: "././opportunity_rover.jpg"
     },
     {
       name: "Spirit",
       launchDate: "10 Jun 2003",
       operator: "NASA",
       missionType: "Rover",
-      img: ""
+      img: "././spirit_rover.png"
     },
     {
       name: "Sojourner",
       launchDate: "4 Dec 1996",
       operator: "NASA",
       missionType: "Rover",
-      img: ""
+      img: "././sojourner_rover.jpg"
     },
     {
       name: "Rosetta",
       launchDate: "2 Mar 2004",
       operator: "ESA",
       missionType: "Gravity Assist",
-      img: ""
+      img: "././rosetta_sat.jpeg"
     }
   ];
 
